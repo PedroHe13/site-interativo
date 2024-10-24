8ccc11ad5ade1dd3c3eb8670a97209397c905caa
PedroHe13 <phrodriguesdon1301@gmail.com>
2024-10-24 18:21:20 -0300

    first up

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'quem é o melhor jogador???',
+    'Lionel Messi'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde o Messi nasceu???',
+    'Rosário, na Argentina'
+)
+
+criaCartao(
+    'curiosidade',
+    'Quantas bolas de ouro ele tem???',
+    'ele tem 8, sendo o jogador com mais bolas de ouro'
+)
+
+criaCartao(
+    'Curiosidade',
+    'qual o nome de seus filhos???',
+    'Iker Casillas, Dani Carvajal, Sergio Ramos, Raphael Varane, Pepe, Marcelo, Álvaro Arbeloa, Fabio Coentrão, Nacho Fernández, Cristiano Ronaldo, Karim Benzema, Gonzalo Higuaín, Luka Modrić, Toni Kroos, Xabi Alonso, Sami Khedira, Isco, Ángel Di María, Mesut Özil, James Rodríguez, Marco Asensio, Lucas Vázquez, Mariano Díaz, Rodrygo Goes, Vinícius Júnior, Eden Hazard, Gareth Bale, Fede Valverde, Eduardo Camavinga, Aurélien Tchouaméni, Casemiro, Lassana Diarra, Mahamadou Diarra, Fernando Gago, David Beckham, Ruud van Nistelrooy, Roberto Carlos, Raul Albiol, Ezequiel Garay, José Antonio Reyes, Robinho, Klaas-Jan Huntelaar, Wesley Sneijder, Esteban Cambiasso, Fernando Hierro, Claude Makélélé, Michel Salgado, Ivan Helguera, Ronaldo Fenômeno, David Alaba, Antonio Rudiger, Theo Hernández, Thibaut Courtois, Keylor Navas, Kiko Casilla, Lucas Silva, Fabio Cannavaro, Javier Saviola, Julio Baptista, Jonathan Woodgate, Jerzy Dudek... Fora os outros q eu não estão na lista💀'
+)
